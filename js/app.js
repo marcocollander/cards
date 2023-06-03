c94ce7813c63f5cb648f3079ff3b3159ab30053e
Zenon Marek Raubuć <m.raubuc@wp.pl>
2023-06-04 00:25:01 +0200

    Added login and signup

--- js/app.js
+++ js/app.js
@@ -148,3 +148,31 @@ referenceDamaKier.addEventListener('click', () => {
   referenceDamaKier.classList.toggle('rotate');
   referenceDamaKier.classList.toggle('rotate-one');
 });
+
+
+const loginBtn = getSelector('#login');
+const signupBtn = getSelector('#signup');
+
+addListener(loginBtn, (e) => {
+  let parent = e.target.parentNode.parentNode;
+  Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up')
+    } else {
+      signupBtn.parentNode.classList.add('slide-up')
+      parent.classList.remove('slide-up')
+    }
+  });
+})
+
+addListener(signupBtn, (e) => {
+  let parent = e.target.parentNode;
+  Array.from(e.target.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up')
+    } else {
+      loginBtn.parentNode.parentNode.classList.add('slide-up')
+      parent.classList.remove('slide-up')
+    }
+  });
+})
