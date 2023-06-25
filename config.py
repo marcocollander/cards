3712e1743e9e87fe7caf5aaceed81b353ff65830
Zenon Marek Raubuć <m.raububc@wp.pl>
2023-06-25 08:51:27 +0200

    Update mail config

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
                    ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # m.raubuc@gmail.com
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # 16 znaków
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # tmgukjsoebqmjfdq
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
